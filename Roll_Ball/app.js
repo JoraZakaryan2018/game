60d0ae73a3dfa2935bce6ef6142320d2c557dcf3
JoraZakaryan2018 <zhora.zakaryan.y@tumo.org>
2018-06-07 23:49:51 +0400

    g

--- Roll_Ball/app.js
+++ Roll_Ball/app.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.redirect("../public/index.html");
+    res.redirect("index.html");
 })
 
 app.listen(3000);
